ab52953b2adf9dc3fb4ad1f4a3520a4d4f571c6f
Rafael dos Santos <rafaelbooster.com@gmail.com>
2025-07-20 19:17:49 +0000

    adicione um padding-top de 3px no link do título

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -20,11 +20,17 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center justify-between px-4 md:px-8">
         <div className="flex items-center">
-          <Link href="/" className="mr-6 flex items-center space-x-3">
+          <Link
+            href="/"
+            className="mr-6 flex items-center space-x-3 pt-[3px]"
+          >
             <span className="font-bold sm:inline-block leading-none">
               LitCode Store
             </span>
-            <span className="h-4 w-px self-center bg-border" aria-hidden="true" />
+            <span
+              className="h-4 w-px self-center bg-border"
+              aria-hidden="true"
+            />
             <span className="font-semibold text-muted-foreground sm:inline-block leading-none">
               TikTok-integrated Web App Store
             </span>
