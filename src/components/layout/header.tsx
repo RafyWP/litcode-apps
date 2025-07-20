352c46ff99c8f2936bfbd727853e900936a8ea1c
Rafael dos Santos <rafaelbooster.com@gmail.com>
2025-07-20 18:30:54 +0000

    o título está alinhado pela base?

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-14 items-center px-4 md:px-8">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-3">
+          <Link href="/" className="mr-6 flex items-baseline space-x-3">
             <span className="font-bold sm:inline-block">
               LitCode Store
             </span>
