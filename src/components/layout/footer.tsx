c7fff4b27cbf4132dca1ebceafdf14bb8d7ef8f8
Rafael dos Santos <rafaelbooster.com@gmail.com>
2025-07-20 23:31:44 +0000

    não bebê, só colocar mx-auto no container ok?

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@
 export default function Footer() {
   return (
     <footer className="w-full py-4 text-xs text-muted-foreground border-t border-border">
-      <div className="container text-center">
+      <div className="container mx-auto text-center">
         © {new Date().getFullYear()}{" "}
         <a
           href="https://litcode.store"
