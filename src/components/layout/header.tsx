bf2cbeded7f0313e10aa19c6f162acb7b073da11
Rafael dos Santos <rafaelbooster.com@gmail.com>
2025-07-20 19:11:03 +0000

    realinhe o header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -44,8 +44,8 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="flex h-14 items-center px-4 md:px-8">
-        <div className="mr-4 flex">
+      <div className="container flex h-14 items-center justify-between px-4 md:px-8">
+        <div className="flex items-center">
           <Link href="/" className="mr-6 flex items-baseline space-x-3">
             <span className="font-bold sm:inline-block">
               LitCode Store
@@ -56,7 +56,7 @@ export default function Header() {
             </span>
           </Link>
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex items-center justify-end">
           {accessToken ? (
             <button
               onClick={handleLogout}
